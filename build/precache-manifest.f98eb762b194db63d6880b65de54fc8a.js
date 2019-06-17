ac035cd3e61e56a2c8fffb06ab367d45c8be7b7a
Logon Aluno <logonrmlocal@fiap.com.br>
2019-06-17 20:26:44 -0300

    first commit

---
+++ build/precache-manifest.f98eb762b194db63d6880b65de54fc8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368dfa2a8c066938bfbb61bf16292468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e16860e1c1f70e06567",
+    "url": "/static/css/main.46e61289.chunk.css"
+  },
+  {
+    "revision": "1bf2f5e85c44557cc225",
+    "url": "/static/js/2.09097db7.chunk.js"
+  },
+  {
+    "revision": "4e16860e1c1f70e06567",
+    "url": "/static/js/main.0731312c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b66159c05a1f7643e234f18bbfbdd86b",
+    "url": "/static/media/mercadolivre.b66159c0.jpg"
+  }
+]);
